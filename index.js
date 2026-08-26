5983aef375e7313f9f6eb84cb3eb4e1bd54e594f
MrPagoza <pf957324@gmail.com>
2026-08-26 02:21:07 -0300

    Change command name from 'Regras' to 'regras'

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ client.once("ready", () => {
 client.on("interactionCreate", async interaction => {
   if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === "Regras") {
+  if (interaction.commandName === "regras") {
     const embed = new EmbedBuilder()
       .setTitle("🤖 Meu Bot")
       .setDescription("Meu primeiro embed está funcionando! 💜")
