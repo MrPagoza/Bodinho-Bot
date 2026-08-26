7c0086df89cb78aa15273d2f9872f96695e6b84e
MrPagoza <pf957324@gmail.com>
2026-08-26 02:33:35 -0300

    Fix typo in import statement for discord.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-zconst { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
+const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
